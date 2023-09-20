75463f1cdcf6496fadc216d0c78ec610a06429e2
DeeGemini <zodiactheegemini@gmail.com>
2023-09-20 15:22:05 -0700

    Entry point

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * main - Entry function
+  * @ac: number of arguments
+  * @av: array of arguments
+  *
+  * Return: 0 on success
+  */
+int main(int ac __attribute__((unused)), char **av __attribute__((unused)))
+{
+
+	loop();
+
+	return (0);
+}
