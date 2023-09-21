c6e244cf170d58bb66a30752d544cd2e6a33e467
DeeGemini <zodiactheegemini@gmail.com>
2023-09-20 18:03:38 -0700

    updated

--- token.c
+++ token.c
@@ -1,7 +1,9 @@
 #include "main.h"
 #include <sys/wait.h>
+
 #define MAX_COM_LENGTH 100
 #define DELIM " \t\r\n\a"
+
 /**
  * tokenise - A function that breaks into tokens
  * @line: String to break into tokens
@@ -9,33 +11,33 @@
  */
 char **tokenise(char *line)
 {
-    int bufsize = MAX_COM_LENGTH;
-    int position = 0;
-    char **tokens = malloc(bufsize * sizeof(char *));
-    char *token;
+	int bufsize = MAX_COM_LENGTH;
+	int position = 0;
+	char **tokens = malloc(bufsize * sizeof(char *));
+	char *token;
 
-    if (!tokens)
-    {
-        perror("Failed to allocate memory");
-        exit(EXIT_FAILURE);
-    }
+	if (!tokens)
+	{
+		perror("Failed to allocate memory");
+		exit(EXIT_FAILURE);
+	}
 
-    for (token = strtok(line, DELIM); token; token = strtok(NULL, DELIM))
-    {
-        tokens[position++] = token;
+	for (token = strtok(line, DELIM); token; token = strtok(NULL, DELIM))
+	{
+		tokens[position++] = token;
 
-        if (position >= bufsize)
-        {
-            bufsize += MAX_COM_LENGTH;
-            tokens = realloc(tokens, bufsize * sizeof(char *));
-            if (!tokens)
-            {
-                perror("Failed to allocate memory");
-                exit(EXIT_FAILURE);
-            }
-        }
-    }
-    tokens[position] = NULL;
-    return (tokens);
+		if (position >= bufsize)
+		{
+			bufsize += MAX_COM_LENGTH;
+			tokens = realloc(tokens, bufsize * sizeof(char *));
+
+			if (!tokens)
+			{
+				perror("Failed to allocate memory");
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+	tokens[position] = NULL;
+	return (tokens);
 }
-
